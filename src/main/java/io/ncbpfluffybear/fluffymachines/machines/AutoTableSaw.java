eb180ce7ad1506f7114cae5674f84951d3d44bdd
zimzaza4 <53421735+zimzaza4@users.noreply.github.com>
2020-12-26 15:52:27 +0800

    Update AutoTableSaw.java

--- src/main/java/io/ncbpfluffybear/fluffymachines/machines/AutoTableSaw.java
+++ src/main/java/io/ncbpfluffybear/fluffymachines/machines/AutoTableSaw.java
@@ -70,7 +70,7 @@ public class AutoTableSaw extends SlimefunItem implements EnergyNetComponent {
             tableSawRecipes.put(new ItemStack(plank), new ItemStack(Material.STICK, 4));
         }
 
-        new BlockMenuPreset(getId(), "&6Auto Table Saw") {
+        new BlockMenuPreset(getId(), "&6自动台锯") {
 
             @Override
             public void init() {
@@ -82,8 +82,8 @@ public class AutoTableSaw extends SlimefunItem implements EnergyNetComponent {
                 if (!BlockStorage.hasBlockInfo(b)
                     || BlockStorage.getLocationInfo(b.getLocation(), "enabled") == null
                     || BlockStorage.getLocationInfo(b.getLocation(), "enabled").equals(String.valueOf(false))) {
-                    menu.replaceExistingItem(6, new CustomItem(Material.GUNPOWDER, "&7Enabled: &4\u2718",
-                        "", "&e> Click to enable this Machine")
+                    menu.replaceExistingItem(6, new CustomItem(Material.GUNPOWDER, "&7已启动: &4\u2718",
+                        "", "&e> 点击启用此机器")
                     );
                     menu.addMenuClickHandler(6, (p, slot, item, action) -> {
                         BlockStorage.addBlockInfo(b, "enabled", String.valueOf(true));
@@ -91,8 +91,8 @@ public class AutoTableSaw extends SlimefunItem implements EnergyNetComponent {
                         return false;
                     });
                 } else {
-                    menu.replaceExistingItem(6, new CustomItem(Material.REDSTONE, "&7Enabled: &2\u2714",
-                        "", "&e> Click to disable this Machine"));
+                    menu.replaceExistingItem(6, new CustomItem(Material.REDSTONE, "&7已启动: &2\u2714",
+                        "", "&e> 点击关闭此机器"));
                     menu.addMenuClickHandler(6, (p, slot, item, action) -> {
                         BlockStorage.addBlockInfo(b, "enabled", String.valueOf(false));
                         newInstance(menu, b);
@@ -156,9 +156,9 @@ public class AutoTableSaw extends SlimefunItem implements EnergyNetComponent {
     protected void constructMenu(BlockMenuPreset preset) {
 
         borders(preset, border, inputBorder, outputBorder);
-        preset.addItem(2, new CustomItem(new ItemStack(Material.STONECUTTER), "&eRecipe", "",
-                "&bPut in the Recipe you want to craft",
-                "&4Table Saw Recipes ONLY"
+        preset.addItem(2, new CustomItem(new ItemStack(Material.STONECUTTER), "&e配方", "",
+                "&b放入您要制作的配方",
+                "&4仅台锯配方"
             ),
             ChestMenuUtils.getEmptyClickHandler());
     }
